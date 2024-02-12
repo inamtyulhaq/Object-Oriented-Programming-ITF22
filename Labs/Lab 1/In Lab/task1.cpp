f8f4c74755039aee010906bb77b73a331f9004f0
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-12 10:34:09 +0500

    Update task1.cpp

--- Labs/Lab 1/In Lab/task1.cpp
+++ Labs/Lab 1/In Lab/task1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+void findCount(string stt)
+{
+    int count = 0;
+
+    for (int i = 0; i < stt.length(); i++) {
+        
+        if (stt[i] == )
+            index++;
+            
+    }
+
+}
+
+int main(){
+
+    string str;
+    cout << "Enter the sentence: ";
+    getline (cin, str);
+
+
+
+
+    return 0;
+}
 
